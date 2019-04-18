8bb89f7d53c2efec2ce9ca9edf77550429d1719e
Lâm Minh Thành <lmt151099@gmail.com>
2019-04-18 20:43:29 +0700

    Added active link logical

--- client/src/components/common/NavBar.js
+++ client/src/components/common/NavBar.js
@@ -2,33 +2,35 @@ import React, { useState } from 'react';
 import {
   MDBNavbar, MDBNavbarNav, MDBNavItem, MDBNavbarToggler, MDBCollapse
   } from "mdbreact";
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-const NavBar = () => {  
-  const [ isOpenMenu, setIsOpenMenu ] = useState(0);
+const isActive = (history, path) => {
+  return history.location.pathname === path ? 'active' : '';
+}
+
+const NavBar = ({history}) => {  
+  const [ isOpen, setIsOpen ] = useState(false);
   const toggleCollapse = () => {
-    setIsOpenMenu(!isOpenMenu);
-  }
+    setIsOpen(!isOpen);
+  }  
   return (
-    <div>
-      <MDBNavbar color="default-color" dark expand="md" fixed="top">
-        <Link to="/">
-          <strong className="white-text">LOGO</strong>
-        </Link>
-        <MDBNavbarToggler onClick={toggleCollapse} />
-        <MDBCollapse id="navbarCollapse3" isOpen={isOpenMenu} navbar>
-          <MDBNavbarNav right>
-            <MDBNavItem>
-              <Link className="nav-link" to="/signin">Sign in</Link>
-            </MDBNavItem>
-            <MDBNavItem>
-              <Link className="nav-link" to="/signup">Sign up</Link>
-            </MDBNavItem>            
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBNavbar>
-    </div>
+    <MDBNavbar color="default-color" dark expand="md" fixed="top">
+      <Link to="/">
+        <strong className="white-text">LOGO</strong>
+      </Link>
+      <MDBNavbarToggler onClick={toggleCollapse} />
+      <MDBCollapse id="navbarCollapse3" isOpen={isOpen} navbar>
+        <MDBNavbarNav right>
+          <MDBNavItem className={isActive(history, '/signin')}>
+            <Link className="nav-link" to="/signin">Sign in</Link>
+          </MDBNavItem>
+          <MDBNavItem className={isActive(history, '/signup')}>
+            <Link className="nav-link" to="/signup">Sign up</Link>
+          </MDBNavItem>            
+        </MDBNavbarNav>
+      </MDBCollapse>
+    </MDBNavbar>
   );
 }
 
-export default NavBar;
+export default withRouter(NavBar);
