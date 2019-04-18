c0e25b2ec3091fe373dae9c5c8b16a329f6aae6a
Lâm Minh Thành <lmt151099@gmail.com>
2019-04-18 19:52:52 +0700

    Change NavBar component from class statefull to function stateless using hook

--- client/src/components/common/NavBar.js
+++ client/src/components/common/NavBar.js
@@ -1,44 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   MDBNavbar, MDBNavbarNav, MDBNavItem, MDBNavbarToggler, MDBCollapse
   } from "mdbreact";
 import { Link } from 'react-router-dom';
 
-class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    };
+const NavBar = () => {  
+  const [ isOpenMenu, setIsOpenMenu ] = useState(0);
+  const toggleCollapse = () => {
+    setIsOpenMenu(!isOpenMenu);
   }
-  toggleCollapse = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  
-  render() {
-    return (
-      <div>
-        <MDBNavbar color="default-color" dark expand="md" fixed="top">
-          <Link to="/">
-            <strong className="white-text">LOGO</strong>
-          </Link>
-          <MDBNavbarToggler onClick={this.toggleCollapse} />
-          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-            <MDBNavbarNav right>
-              <MDBNavItem>
-                <Link className="nav-link" to="/signin">Sign in</Link>
-              </MDBNavItem>
-              <MDBNavItem>
-                <Link className="nav-link" to="/signup">Sign up</Link>
-              </MDBNavItem>            
-            </MDBNavbarNav>
-          </MDBCollapse>
-        </MDBNavbar>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <MDBNavbar color="default-color" dark expand="md" fixed="top">
+        <Link to="/">
+          <strong className="white-text">LOGO</strong>
+        </Link>
+        <MDBNavbarToggler onClick={toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={isOpenMenu} navbar>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              <Link className="nav-link" to="/signin">Sign in</Link>
+            </MDBNavItem>
+            <MDBNavItem>
+              <Link className="nav-link" to="/signup">Sign up</Link>
+            </MDBNavItem>            
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    </div>
+  );
 }
 
 export default NavBar;
